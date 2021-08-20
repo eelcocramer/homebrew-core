8fdac82d8b2b745d853c086efc1ddf67b26ee3da
Eelco <eelco@hailendal.org>
2021-08-20 11:04:55 +0200

    go 1.17

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://golang.org"
-  url "https://golang.org/dl/go1.16.6.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.16.6.src.tar.gz"
-  sha256 "a3a5d4bc401b51db065e4f93b523347a4d343ae0c0b08a65c3423b05a138037d"
+  url "https://golang.org/dl/go1.17.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.17.src.tar.gz"
+  sha256 "3a70e5055509f347c0fb831ca07a2bf3b531068f349b14a3c652e9b5b67beb5d"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git"
 
@@ -24,20 +24,20 @@ class Go < Formula
   resource "gobootstrap" do
     on_macos do
       if Hardware::CPU.arm?
-        url "https://storage.googleapis.com/golang/go1.16.darwin-arm64.tar.gz"
-        version "1.16"
-        sha256 "4dac57c00168d30bbd02d95131d5de9ca88e04f2c5a29a404576f30ae9b54810"
+        url "https://storage.googleapis.com/golang/go1.17.darwin-arm64.tar.gz"
+        version "1.17"
+        sha256 "da4e3e3c194bf9eed081de8842a157120ef44a7a8d7c820201adae7b0e28b20b"
       else
-        url "https://storage.googleapis.com/golang/go1.16.darwin-amd64.tar.gz"
-        version "1.16"
-        sha256 "6000a9522975d116bf76044967d7e69e04e982e9625330d9a539a8b45395f9a8"
+        url "https://storage.googleapis.com/golang/go1.17.darwin-amd64.tar.gz"
+        version "1.17"
+        sha256 "355bd544ce08d7d484d9d7de05a71b5c6f5bc10aa4b316688c2192aeb3dacfd1"
       end
     end
 
     on_linux do
-      url "https://storage.googleapis.com/golang/go1.16.linux-amd64.tar.gz"
-      version "1.16"
-      sha256 "013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2"
+      url "https://storage.googleapis.com/golang/go1.17.linux-amd64.tar.gz"
+      version "1.17"
+      sha256 "6bf89fc4f5ad763871cf7eac80a2d594492de7a818303283f1366a7f6a30372d"
     end
   end
 
